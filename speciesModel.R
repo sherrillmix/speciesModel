a865522153e5b369c961e1c585de19aab6e3a0c7
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-17 17:18:36 -0400

    Fit on real data. Add multicore

--- speciesModel.R
+++ speciesModel.R
@@ -1,13 +1,19 @@
 library(rstan)
 library(vipor)
-#add a regression parameter while retaining groups
+
+#use multiple cores (careful with big models on small computers)
+options(mc.cores = parallel::detectCores())
+#cache compilations (doesn't actually work that well, at least with model_code=)
+rstan_options(auto_write = TRUE)
+
+
 stanCode<-'
 data{
   int<lower=0> n;
-  real x[n]; 
+  real otus[n]; //log
   int<lower=1> ids[n];
   int<lower=0> nIds;
-  vector<lower=0>[nIds] weights;
+  vector[nIds] weights; //log
   int<lower=1> classes[nIds];
   int<lower=0> nClasses;
 }
@@ -32,9 +38,10 @@ model {
   metaBetaSd ~ gamma(1,1);
   metaSigma ~ gamma(1,1);
   rawMus ~ normal(0,1);
-  x ~ normal(mus[ids],sigmas[ids]);
+  otus ~ normal(mus[ids],sigmas[ids]);
 }
 '
+
 nSpecies<-20
 mus<-rnorm(nSpecies,3,1)
 weights<-rgamma(nSpecies,1,.1)
@@ -51,7 +58,7 @@ plot(xWeights,unlist(x),pch=21,bg=rainbow(max(ids))[ids])
 fit<-stan(
   model_code=stanCode,
   data=list(
-    x=unlist(x),
+    otus=unlist(x),
     n=length(unlist(x)),
     ids=ids,
     nIds=length(x),
@@ -61,6 +68,31 @@ fit<-stan(
   ),
   control=list(adapt_delta=.95),
   chains=6,iter=3000 # get a few extra samples as an example
+)
+print(fit)
+
+plot(fit)
+print(traceplot(fit))
+sims<-extract(fit)
+hist(sims[['metaBeta']])
+
+dat<-read.csv('data2.csv',row.names=1)
+dat$speciesId<-as.numeric(as.factor(dat$host_species_common_name))
+dat$classId<-as.numeric(as.factor(dat$class))
+
+fit<-stan(
+  model_code=stanCode,
+  data=list(
+    otus=dat$log.otu,
+    n=nrow(dat),
+    ids=dat$speciesId,
+    nIds=max(dat$speciesId),
+    weights=tapply(dat$log.weight,dat$speciesId,mean),
+    classes=tapply(dat$classId,dat$speciesId,'[[',1),
+    nClasses=max(dat$classId)
+  ),
+  control=list(adapt_delta=.99),
+  chains=8,iter=3000,thin=10
 )
 print(fit)
 
